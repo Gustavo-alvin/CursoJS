dde0ab186e4dea88cbc1a8330c2ecc6ccc856ce6
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-04 12:18:06 -0300

    update

---
+++ Aula 4/comentarios.js
@@ -0,0 +1,12 @@
+//E escrever um comentário longo
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+
+/*
+Esse tipo de comentario permite 
+comentarios em blocos 
+*/
+
+// Vai ser ignorado
