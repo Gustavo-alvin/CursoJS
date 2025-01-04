5f53ac5ad8966f79371411f85ab62bf9652cf226
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-04 12:30:10 -0300

    update

---
+++ Aula 6/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não pode conter espacços ou traços
+// Utilizamos camelCase
+// case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não UTILIZE Var, UTILIZE LET.
+
+let nomeCliente = "Gustavo";
+nomeCliente = "Alves";
+
+console.log(nomeCliente);
