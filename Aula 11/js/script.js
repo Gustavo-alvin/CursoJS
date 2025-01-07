bcc745ac803502511c853619877db792c8414f94
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-06 23:49:23 -0300

    updates

---
+++ Aula 11/js/script.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Digite seu número");
+let num2 = prompt("Digite outro número");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let resultado = num1 + num2;
+
+alert(`Esse é o resultado da soma ${resultado}`);
+
+console.log(num1+num2);
