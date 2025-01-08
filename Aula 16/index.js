02c90ce5bcb0c99ba7950574a614cbdbbbe80388
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-08 19:13:50 -0300

    update

---
+++ Aula 16/index.js
@@ -0,0 +1,29 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos instanceof Array)
+
+// alunos.push('Gustavo');
+// alunos.push('Luana')
+// console.log(alunos.slice(0, 2));
+
+// delete alunos[1]; // deleta um item sem mudar sua ordem
+
+//const removido = alunos.pop(); // Remove o item do final
+//const removido = alunos.shift(); // remove o primeiro item
+
+// alunos.unshift('Gustavo'); // Adiciona no começo da ARRAY
+// alunos.unshift('Luana');
+
+// alunos.push('Gusavo'); // Adiciona mais um item no array
+// alunos.push('Alves');
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona mais um item no array
+// alunos[alunos.length] = 'Fabio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza'
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
