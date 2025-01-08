26b6cb7396585e1cd71250e5221e8290edb1cf55
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-08 13:42:28 -0300

    update

---
+++ Aula 15/index.js
@@ -0,0 +1,10 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1); arredonda o numero pra baixo
+//let num2 = Math.ceil(num1); arredonda o numero pra cima
+//let num2 = Math.round(num1); arredonda o numero pra a csa mais proxima
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+const aleatorio = Math.round(Math.random() * (10 - 5) +5);
+
+console.log(aleatorio)
+
