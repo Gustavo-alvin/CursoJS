277b7459e37fc44559b5b9d8ca1503abbcb9bc10
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-09 15:33:41 -0300

    up

--- Aula 23/index.js
+++ Aula 23/index.js
@@ -9,6 +9,24 @@
  * NaN
  */
 
+const a = null || "valor padrão";
+console.log(a); // "valor padrão"
+// Explicação: null é falsy, então o "curto-circuito" ocorre no "valor padrão".
+
+const b = 0 || false || "texto";
+console.log(b); // "texto"
+// Explicação: 0 e false são falsy, então ele retorna "texto".
+
+
+// const a = "valor" && 0;
+// console.log(a); // 0
+// // Explicação: "valor" é truthy, mas 0 é falsy, então o curto-circuito ocorre no 0.
+
+// const b = true && "JavaScript";
+// console.log(b); // "JavaScript"
+// Explicação: Ambos são truthy, então retorna o último valor.
+
+
 
 
 
