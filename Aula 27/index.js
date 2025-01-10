699a8734d79c2661bdd13cde402df2cc6bbf58ab
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-10 20:49:17 -0300

    update

---
+++ Aula 27/index.js
@@ -0,0 +1,17 @@
+// operação ternária
+// (condição)? 'Valor para verdadeiro' : 'Valor para falso';
+
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'preta';
+
+console.log(nivelUsuario, corPadrao);
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuário normal');
+// }
