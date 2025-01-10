064240a09815328f9f2069ac9453a0afa8589e9a
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2025-01-10 19:18:32 -0300

    updates

---
+++ Pratica/Desafio 2/assets/js/main.js
@@ -0,0 +1,8 @@
+const num1 = prompt("Digite algum némero");
+const num2 = prompt("Digite outro número");
+
+if (num1 > num2) {
+    alert(" O primeiro número é maior.");
+}  else {
+    alert("O segundo número é maior ou igual.");
+}
